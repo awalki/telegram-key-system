c8868bde7746dc3ae831d275eabc162505cce1fc
Nazar <je3nsoff@icloud.com>
2024-08-18 04:50:54 +0300

    Code refactoring

--- src/telegram/state.py
+++ src/telegram/state.py
@@ -17,17 +17,18 @@ class KeySG(StatesGroup):
 
 def key_check(text: str):
     key = select_key(text)
+    is_key_activated = key_activated(text)
 
-    if key and not key_activated(text):
+    if key and not is_key_activated:
         return text
     raise ValueError
 
 
 async def success_handler(message: Message, widget: ManagedTextInput, dialog_manager: DialogManager, text: str) -> None:
-    start_data = dialog_manager.start_data["user_id"]
+    user_id = dialog_manager.start_data["user_id"]
 
-    await message.answer(text=f'The key has been activated, it was tied to your account\n\nYour ID: {start_data}')
-    edit_column_values(text, start_data)
+    await message.answer(text=f'The key has been activated, it was tied to your account\n\nYour ID: {user_id}')
+    edit_column_values(text, user_id)
 
     await dialog_manager.done()
 
@@ -45,7 +46,7 @@ async def cancel_click_process(callback: CallbackQuery,
     await callback.message.edit_text("Canceled")
     await dialog_manager.mark_closed()
 
-async def username_getter(dialog_manager: DialogManager, event_from_user: User, **kwargs):
+async def user_getter(dialog_manager: DialogManager, event_from_user: User, **kwargs):
     return {
         "username": event_from_user.username,
         "user_id": event_from_user.id
@@ -53,7 +54,7 @@ async def username_getter(dialog_manager: DialogManager, event_from_user: User, 
 
 key_dialog = Dialog(
     Window(
-        Format("Welcome to the key system\n\nEnter an access key: "),
+        Format("{username} ({user_id})\nWelcome to the key system\n\nEnter an access key: "),
         TextInput(
             id="key",
             type_factory=key_check,
@@ -65,7 +66,7 @@ key_dialog = Dialog(
             id="cancel",
             on_click=cancel_click_process,
         ),
-        getter=username_getter,
+        getter=user_getter,
         state=KeySG().key,
     ),
 )
